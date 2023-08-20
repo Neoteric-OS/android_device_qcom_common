d4731e7bc877c2be512800038aed5875da138e0f
HELLBOY017 <abhaygill017@gmail.com>
2023-08-20 14:13:03 +0530

    common: Bring back flipendo sepolicy

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -28,3 +28,7 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(COMMON_SEPOLICY_PATH)/common/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(COMMON_SEPOLICY_PATH)/common/public
+    
+# Zeph specific sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += \
+        $(COMMON_SEPOLICY_PATH)/pixel/vendor
