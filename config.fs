d747a40857210730ff3872e1231941e016d99ab4
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-04-14 20:23:27 +0000

    config.fs: Update to LA.VENDOR.14.3.0.r1-11500-lanai.0
    Change-Id: I1e97dd39985305d6016109ae18bb05924c848dea

--- config.fs
+++ config.fs
@@ -25,9 +25,6 @@ value:2908
 [AID_VENDOR_QTR]
 value:2909
 
-[AID_VENDOR_QCC]
-value:2914
-
 [AID_VENDOR_NXP_STRONGBOX]
 value:2910
 
@@ -39,6 +36,15 @@ value:2912
 
 [AID_VENDOR_THALES_STRONGBOX]
 value:2913
+
+[AID_VENDOR_QCC]
+value:2914
+
+[AID_VENDOR_NXP_AUTHSECRET]
+value:2915
+
+[AID_VENDOR_THALES_WEAVER]
+value:2916
 
 [vendor/bin/hw/android.hardware.security.keymint-service.strongbox-thales]
 mode: 0755
@@ -55,6 +61,12 @@ caps: SYS_ADMIN SYS_NICE
 [vendor/bin/hw/android.hardware.weaver@1.0-service]
 mode: 0755
 user: AID_VENDOR_NXP_WEAVER
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.weaver-service.thales]
+mode: 0755
+user: AID_VENDOR_THALES_WEAVER
 group: AID_SYSTEM
 caps: SYS_ADMIN SYS_NICE
 
@@ -99,7 +111,6 @@ mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
-
 [vendor/bin/hw/android.hardware.bluetooth@1.1-service-qti-lazy]
 mode: 0755
 user: AID_BLUETOOTH
