3caaaa554a6658b57b33c5121911ca39bf1f1ba0
Abhay Singh Gill <abhaygill017@gmail.com>
2025-07-03 14:40:26 +0530

    common: Nuke flipendo sepolicy
    No longer using aospa gms.
    
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- sepolicy/SEPolicy.mk
+++ sepolicy/SEPolicy.mk
@@ -36,7 +36,5 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     $(COMMON_SEPOLICY_PATH)/generic/public
 
 # Neoteric-QCOM Specific Required SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(COMMON_SEPOLICY_PATH)/pixel/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(COMMON_SEPOLICY_PATH)/neoteric/private
